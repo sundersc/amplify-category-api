e337a3fd9f49317e9604e0513a1addf79cf12193
Christopher Sundersingh <sundersc@amazon.com>
2022-07-15 11:56:33 -0700

    remove input object for rds

--- packages/amplify-category-api/src/provider-utils/awscloudformation/service-walkthroughs/appSync-walkthrough.ts
+++ packages/amplify-category-api/src/provider-utils/awscloudformation/service-walkthroughs/appSync-walkthrough.ts
@@ -451,7 +451,7 @@ export const serviceWalkthrough = async (context: $TSContext, serviceMetadata: $
 
   const { templateSelection } = await inquirer.prompt(templateSelectionQuestion);
   const schemaFilePath = path.join(graphqlSchemaDir, templateSelection);
-  schemaContent += transformerVersion === 2 ? defineGlobalSandboxMode(getGraphQLTransformerAuthDocLink(transformerVersion)) : '';
+  schemaContent += (transformerVersion === 2 && basicInfoAnswers.storage.type !== 'RDS') ? defineGlobalSandboxMode(getGraphQLTransformerAuthDocLink(transformerVersion)) : '';
   schemaContent += fs.readFileSync(schemaFilePath, 'utf8');
 
   return {
